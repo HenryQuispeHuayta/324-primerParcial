a68d9114eacf050b1e7db3f0431bcc0419be9472
Henry Quispe Huayta <henry.quispe.huayta@gmail.com>
2022-04-25 07:02:24 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primer Parcial</title>
+</head>
+<body>
+    <?php
+        include "conexion.php";
+        // incluirAcceso(4,'andrea','andrea');
+        incluirInscripcion(1,'INF-325',20,20,25);
+        // incluirPersona(1,'henry QH','1994-03-20',02);
+    ?>
+</body>
+</html>
